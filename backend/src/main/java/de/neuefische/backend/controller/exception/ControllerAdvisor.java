24f15a145df3d88b6de30def1087f26f714b38d7
Matthias Hiemer <matthias.hiemer@neuefische.de>
2021-10-20 10:44:39 +0200

    Add initial implementation of Login controller with dummy JWT

--- backend/src/main/java/de/neuefische/backend/controller/exception/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/backend/controller/exception/ControllerAdvisor.java
@@ -3,6 +3,7 @@ package de.neuefische.backend.controller.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -29,6 +30,15 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
         ApiError apiError = new ApiError("Given input is not processable!", ex.getMessage());
 
         return new ResponseEntity<>(apiError, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex){
+        log.error("Username and password are not valid!", ex);
+
+        ApiError apiError = new ApiError("Username and password are not valid!", ex.getMessage());
+
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Throwable.class)
