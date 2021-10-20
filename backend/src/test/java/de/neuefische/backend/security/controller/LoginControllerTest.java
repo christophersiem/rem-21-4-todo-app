91ceeb76fb92b7723dddffec5422635aed580ab5
Matthias Hiemer <matthias.hiemer@neuefische.de>
2021-10-20 15:40:25 +0200

    Add AuthFilter to validate JWT

--- backend/src/test/java/de/neuefische/backend/security/controller/LoginControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/security/controller/LoginControllerTest.java
@@ -37,12 +37,12 @@ class LoginControllerTest {
 
         // GIVEN
         appUserRepo.save(AppUser.builder()
-                .username("user")
+                .username("test-user")
                 .password(passwordEncoder.encode("some-password"))
                 .build());
 
         // WHEN
-        AppUser appUser = new AppUser("user", "some-password");
+        AppUser appUser = new AppUser("test-user", "some-password");
         ResponseEntity<String> response = restTemplate.postForEntity("/auth/login", appUser, String.class);
 
         // THEN
@@ -53,7 +53,7 @@ class LoginControllerTest {
                 .setSigningKey(JWT_SECRET)
                 .parseClaimsJws(response.getBody())
                 .getBody();
-        assertThat(body.getSubject(), is("user"));
+        assertThat(body.getSubject(), is("test-user"));
     }
 
     @Test
