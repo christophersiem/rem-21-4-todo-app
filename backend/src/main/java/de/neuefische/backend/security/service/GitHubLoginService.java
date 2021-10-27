a469037e36107c03a4781b9fdb5ef22badd10f98
Matthias Hiemer <matthias.hiemer@neuefische.de>
2021-10-27 11:43:19 +0200

    Add JWT creation

--- backend/src/main/java/de/neuefische/backend/security/service/GitHubLoginService.java
+++ backend/src/main/java/de/neuefische/backend/security/service/GitHubLoginService.java
@@ -4,13 +4,17 @@ import de.neuefische.backend.security.api.GitHubApiService;
 import de.neuefische.backend.security.model.GitHubUserDto;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 @Service
 public class GitHubLoginService {
 
     private final GitHubApiService gitHubApiService;
+    private final JWTUtilService jwtUtilService;
 
-    public GitHubLoginService(GitHubApiService gitHubApiService) {
+    public GitHubLoginService(GitHubApiService gitHubApiService, JWTUtilService jwtUtilService) {
         this.gitHubApiService = gitHubApiService;
+        this.jwtUtilService = jwtUtilService;
     }
 
     public String verifyGitHubCode(String code) {
@@ -21,8 +25,6 @@ public class GitHubLoginService {
         GitHubUserDto gitHubUserDto = gitHubApiService.retrieveUserInfo(gitHubToken);
 
         // 3) Create JWT access token
-
-        // TODO
-        return "JWT!";
+        return jwtUtilService.createToken(new HashMap<>(), gitHubUserDto.getLogin());
     }
 }
