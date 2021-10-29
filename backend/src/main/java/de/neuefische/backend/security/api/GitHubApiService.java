2d7cb33eb17e774ca54206159aeecc87686d2244
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-10-29 13:05:42 +0200

    Suppress NullPointer Warning since we check for null

--- backend/src/main/java/de/neuefische/backend/security/api/GitHubApiService.java
+++ backend/src/main/java/de/neuefische/backend/security/api/GitHubApiService.java
@@ -4,7 +4,6 @@ import de.neuefische.backend.controller.exception.GitHubAuthException;
 import de.neuefische.backend.security.model.GitHubAccessTokenDto;
 import de.neuefische.backend.security.model.GitHubOAuthCredentialsDto;
 import de.neuefische.backend.security.model.GitHubUserDto;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -17,7 +16,7 @@ import java.util.List;
 public class GitHubApiService {
 
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
 
     private static final String GITHUB_CODE_URL = "https://github.com/login/oauth/access_token";
@@ -34,6 +33,7 @@ public class GitHubApiService {
         this.restTemplate = restTemplate;
     }
 
+    @SuppressWarnings({"java:S2259"})
     public String retrieveGitHubToken(String code) {
 
         GitHubOAuthCredentialsDto credentialsDto = GitHubOAuthCredentialsDto.builder()
@@ -51,7 +51,7 @@ public class GitHubApiService {
                 new HttpEntity<>(credentialsDto, httpHeaders),
                 GitHubAccessTokenDto.class);
 
-        if(response.getBody() == null){
+        if (response.getBody() == null) {
             throw new GitHubAuthException("Error while authenticating with code via GitHub! Body is null!");
         }
 
@@ -63,12 +63,12 @@ public class GitHubApiService {
         httpHeaders.setBearerAuth(gitHubToken);
 
         ResponseEntity<GitHubUserDto> response = restTemplate.exchange(
-               GITHUB_USER_URL,
+                GITHUB_USER_URL,
                 HttpMethod.GET,
                 new HttpEntity<>(httpHeaders),
                 GitHubUserDto.class);
 
-        if(response.getBody() == null){
+        if (response.getBody() == null) {
             throw new GitHubAuthException("Error while authenticating with code via GitHub! Body is null!");
         }
 
