42a9b3d67127a40efad2888fab8b069658e5b31d
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-10-29 12:40:00 +0200

    ReactRouting GetMapping, CSRF disable only for backend routes

--- backend/src/main/java/de/neuefische/backend/config/ReactRoutingForward.java
+++ backend/src/main/java/de/neuefische/backend/config/ReactRoutingForward.java
@@ -1,12 +1,13 @@
 package de.neuefische.backend.config;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReactRoutingForward {
 
-    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    @GetMapping(value = "/**/{[path:[^\\.]*}")
     public String forwardToRoutUrl() {
         return "forward:/";
     }
--- backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
@@ -46,9 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf().ignoringAntMatchers("/auth/**", "/api/**").and()
                 .authorizeRequests()
-                .antMatchers("/auth/login").permitAll()
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/**").permitAll()
                 .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
